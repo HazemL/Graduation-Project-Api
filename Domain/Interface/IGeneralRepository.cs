a6cc8a790f9ff9e96716aa76a4cb0ca7e799d24c
Mohamed Hussine Thabet <Mohamed.20353647@compit.aun.edu.eg>
2025-12-13 22:23:11 +0200

    interface for crud

---
+++ Domain/Interface/IGeneralRepository.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Domain.Interface
+{
+    public interface IGeneralRepository<T> where T : BaseModelEntity
+    {
+        public IQueryable<T> GetAll();
+        public Task<T> GetByID(int id);
+        public Task<T> GetByIDWithTracking(int id);
+        public Task Add(T entity);
+        public Task<bool> IsExist(int id);
+
+
+        public Task Update(T entity);
+        public Task Delete(int id);
+        public void UpdateInclude(T entity , params string[] modifiedParams);
+
+    }
+}
